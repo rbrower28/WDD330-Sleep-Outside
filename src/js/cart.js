821b3afa862d4283bc4661042d17c540da15a21a
Ryan Brower <ryan.brower28@gmail.com>
2022-04-21 18:50:37 -0700

    fixed linting and format errors

--- src/js/cart.js
+++ src/js/cart.js
@@ -3,15 +3,14 @@ function getLocalStorage(key) {
 }
 
 function getCartContents() {
-  let markup = '';
-  const cartItems = getLocalStorage('so-cart');
+  const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  document.querySelector(".product-list").innerHTML = htmlItems.join("");
   // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
 function renderCartItem(item) {
-const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -25,7 +24,6 @@ const newItem = `<li class="cart-card divider">
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
-  console.log(newItem);
   return newItem;
 }
 
